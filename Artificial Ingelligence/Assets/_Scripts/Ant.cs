1c5e09880616c519116c187892624bf507f5ac81
Daan de Bruijn <ddebruijn9@gmail.com>
2019-03-21 17:40:26 +0100

    Tussentijdse Push
    Going home. Moved the tasks to the Unit Class so I dont have to write them twice. (both AI's will be using part of this behavior tree).

--- Artificial Ingelligence/Assets/_Scripts/Ant.cs
+++ Artificial Ingelligence/Assets/_Scripts/Ant.cs
@@ -1,59 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Panda;
 
 public class Ant : Unit {
 
     TileGrid grid;
-
-    [Task]
-    void RequestFoodPath() {
-        if (!carrying) {
-            PathRequestManager.RequestPath(transform.position, targetFood.position, OnPathFound);
-            Task.current.Succeed();
-        }
-        else if (carrying) {
-            Task.current.Fail();
-        }
-    }
-    [Task]
-    void RequestAntPath() {
-        if (!carrying) {
-            // PathRequestManager.RequestPath(transform.position, targetAnt.position, OnPathFound);
-            Task.current.Succeed();
-        }
-        else if (carrying) {
-            Task.current.Fail();
-        }
-    }
-    [Task]
-    void RequestPathHome() {
-        PathRequestManager.RequestPath(transform.position, targetHome.position, OnPathFound);
-        Task.current.Succeed();
-    }
-    [Task]
-    void Flee() {
-        PathRequestManager.RequestPath(transform.position, Random.insideUnitCircle * 3, OnPathFound);
-        Task.current.Succeed();
-    }
-    [Task]
-    void Hide() {   // Checks if there are any hiding spaces in the vicinity. If so, it hides. Else, the task fails.
-        Collider[] cols = Physics.OverlapBox(transform.position, Vector3.one, Quaternion.identity);
-        List<Transform> hidingSpaces = new List<Transform>();
-        foreach (Collider collider in cols) {
-            if (collider.CompareTag("Obstacle")) {
-                hidingSpaces.Add(collider.transform);
-            }
-        }
-        if (hidingSpaces.Count > 0) {
-            PathRequestManager.RequestPath(transform.position, hidingSpaces[Random.Range(0, hidingSpaces.Count)].position, OnPathFound);
-            Task.current.Succeed();
-        }
-        else if (hidingSpaces.Count <= 0) {
-            Task.current.Fail();
-        }
-    }
 
     private void Start() {
         targetFood = GameObject.FindGameObjectWithTag("Food").transform;
--- Artificial Ingelligence/Assets/_Scripts/Unit.cs
+++ Artificial Ingelligence/Assets/_Scripts/Unit.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Panda;
 
 public class Unit : MonoBehaviour
 {
@@ -53,4 +54,55 @@ public class Unit : MonoBehaviour
             }
         }
     }
+
+    [Task]
+    void RequestFoodPath() {    // Requests a path to the objective.
+        if (!carrying) {
+            PathRequestManager.RequestPath(transform.position, targetFood.position, OnPathFound);
+            Task.current.Succeed();
+        }
+        else if (carrying) {
+            Task.current.Fail();
+        }
+    }
+    [Task]
+    void RequestAntPath() { // Requests a path to a dead ant.
+        if (!carrying) {
+            // PathRequestManager.RequestPath(transform.position, targetAnt.position, OnPathFound); (Will be dead ant position).
+            Task.current.Succeed();
+        }
+        else if (carrying) {
+            Task.current.Fail();
+        }
+    }
+    [Task]
+    void RequestPathHome() {    // Requests a path to the Homebase.
+        PathRequestManager.RequestPath(transform.position, targetHome.position, OnPathFound);
+        Task.current.Succeed();
+    }
+    [Task]
+    void Flee() {   // Unit Flees to a random direction.
+        PathRequestManager.RequestPath(transform.position, Random.insideUnitCircle * 3, OnPathFound);
+        Task.current.Succeed();
+    }
+    [Task]
+    void Hide() {   // Checks if there are any hiding spaces in the vicinity. If so, it hides. Else, the task fails.
+        List<Transform> hidingSpaces = new List<Transform>();
+
+        Collider[] cols = Physics.OverlapBox(transform.position, Vector3.one, Quaternion.identity);
+        foreach (Collider collider in cols) {
+            if (collider.CompareTag("Obstacle")) {
+                hidingSpaces.Add(collider.transform);
+            }
+        }
+
+        if (hidingSpaces.Count > 0) {
+            PathRequestManager.RequestPath(transform.position, hidingSpaces[Random.Range(0, hidingSpaces.Count)].position, OnPathFound);
+            Task.current.Succeed();
+        }
+        else if (hidingSpaces.Count <= 0) {
+            Task.current.Fail();
+        }
+    }
+
 }
